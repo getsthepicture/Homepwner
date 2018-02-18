5e117abdd11ed4230f6d49ad03898f6dd6d820ed
Laurence Wingo <qaneuro@Laurences-MacBook-Pro-3.local>
2018-02-18 16:49:07 -0500

    Added the Item.swift file in order to create the model of the application

--- Homepwner/Item.swift
+++ Homepwner/Item.swift
@@ -1,16 +1,9 @@
 //
-//  ItemsViewController.swift
+//  Item.swift
 //  Homepwner
 //
 //  Created by Laurence Wingo on 2/18/18.
 //  Copyright © 2018 Laurence Wingo. All rights reserved.
 //
 
-import UIKit
-
-class ItemsViewController: UITableViewController {
-    
-    
-    
-}
-
+import Foundation
